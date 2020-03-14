44b42eb7bb110062c5c962b3e56d6865e4bb4dba
jkL970910 <jkliu97@gmail.com>
2020-03-14 19:15:48 +0800

    proj1gold Finised

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,77 @@
+import        static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.List;
+import java.util.ArrayList;
+
+//@source StudentArrayDequeLauncher
+public class TestArrayDequeGold {
+    @Test
+    public void testArrayDequeTask1() {
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+
+        //test addLast
+        for (int i = 0; i < 10; i++) {
+            //create an int random number range from 0 to n
+            int random = StdRandom.uniform(100);
+            ads.addLast(random);
+            sad.addLast(random);
+        }
+        for (int i = 0; i < 10; i++) {
+            int actual = sad.get(i);
+            int expected = ads.get(i);
+            //assertEquals(String message, int expected, int actual)
+            //if the compare returns true, the message won't be mentioned
+            assertEquals("Error!\nThe addLast() test Failed:\n Random Number " + expected
+                    + " is not equal to " + actual + " !", expected, actual);
+        }
+
+        //test addFirst
+        for (int i = 0; i < 10; i++) {
+            //create an int random number range from 0 to n
+            int random = StdRandom.uniform(100);
+            ads.addFirst(random);
+            sad.addFirst(random);
+        }
+        for (int i = 0; i < 10; i++) {
+            int actual = sad.get(i);
+            int expected = ads.get(i);
+            //assertEquals(String message, int expected, int actual)
+            //if the compare returns true, the message won't be mentioned
+            assertEquals("Error!\nThe addFirst() test Failed:\n Random Number " + expected
+                    + " is not equal to " + actual + " !", expected, actual);
+        }
+
+        //test removeFirst
+        List<Integer> actualList = new ArrayList<>();
+        List<Integer> expectedList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            actualList.add(sad.removeFirst());
+            expectedList.add(ads.removeFirst());
+        }
+        for (int i = 0; i < 10; i++) {
+            int actual = sad.get(i);
+            int expected = ads.get(i);
+            assertEquals("Error!\nThe removeFirst() Failed: \n Random number " + expected
+                    + " is not equal to " + actual + " !", expected, actual);
+        }
+
+        //test removeLast
+        actualList.clear();
+        expectedList.clear();
+        for (int i = 0; i < 10; i++) {
+            actualList.add(sad.removeLast());
+            expectedList.add(ads.removeLast());
+        }
+        int actual = sad.size();
+        int expected = ads.size();
+        assertEquals("Error!\nThe removeLast() test size " + actual
+                + " is not equal to " + expected + " !", expected, actual);
+        for (int i = 0; i < 10; i++) {
+            assertEquals("Error!\nThe removeLast() test Failed:\n Random number "
+                    + expectedList.get(i) + " is not equal to " + actualList.get(i)
+                    + " !", expectedList.get(i), actualList.get(i));
+        }
+
+    }
+}
