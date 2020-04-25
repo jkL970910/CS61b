0c9a12794384d732307585765562aa0d819b85c7
jkL970910 <jkliu97@gmail.com>
2020-04-25 21:16:13 +0800

    hw2 finished

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,154 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private final WeightedQuickUnionUF uf;
+    private int countOpen = 0;
+    private final int n;
+    private boolean[] siteopen;
+
+    /*
+     * Create two virtual sites: virtualTop and virtualBottom
+     * Every site has two status stored in the connectTop and
+     * connectBottom, indicates whether the site is linked to
+     * the top or the bottom.
+     * Percolates if virtualTop is connected to virtualBottom.
+    */
+    private boolean[] isconnectTop;
+    private boolean[] isconnectBottom;
+    private boolean percolatesFlag = false;
+
+    //create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        check(N);
+        this.uf = new WeightedQuickUnionUF(N * N);
+        this.n = N;
+        //the default site is blocked
+        this.siteopen = new boolean[N * N];
+        //two virtual sites
+        this.isconnectBottom = new boolean[N * N];
+        this.isconnectTop = new boolean[N * N];
+    }
+
+    //open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        check(row, col);
+        int index = xyTo1D(row, col);
+
+        //open the non-open site
+        if(!siteopen[index]) {
+            siteopen[index] = true;
+            countOpen += 1;
+        }
+        boolean virtualTop = false;
+        boolean virtualBottom = false;
+        // if the site is on the first row, connect it to the top
+        if (row == 0) {
+            virtualTop = true;
+        }
+        // if the site is on the last row, connect it to the bottom
+        if (row == n - 1) {
+            virtualBottom = true;
+        }
+
+        // then check and connect the site with its neighbors
+        // union the site with left neighbor
+        if (col > 0 && siteopen[index - 1]) {
+            //if the neighbor is full or site is full, validate the virtualTop
+            if (!virtualTop && isconnectTop[uf.find(index - 1)] || isconnectTop[uf.find(index)]) {
+                virtualTop = true;
+            }
+            // if the neighbor is connect bottom or the site is, validate the virtualBottom
+            if (!virtualBottom && isconnectBottom[uf.find(index - 1)] || isconnectBottom[uf.find(index)]) {
+                virtualBottom = true;
+            }
+            uf.union(index, index - 1);
+        }
+        // union the site with right neighbor
+        if (col < n - 1 && siteopen[index + 1]) {
+            //if the neighbor is full or site is full, validate the virtualTop
+            if (!virtualTop && isconnectTop[uf.find(index + 1)] || isconnectTop[uf.find(index)]) {
+                virtualTop = true;
+            }
+            // if the neighbor is connect bottom or the site is, validate the virtualBottom
+            if (!virtualBottom && isconnectBottom[uf.find(index + 1)] || isconnectBottom[uf.find(index)]) {
+                virtualBottom = true;
+            }
+            uf.union(index, index + 1);
+        }
+        // union the site with upper neighbor
+        if (row > 0 && siteopen[index - n]) {
+            //if the neighbor is full or site is full, validate the virtualTop
+            if (!virtualTop && isconnectTop[uf.find(index - n)] || isconnectTop[uf.find(index)]) {
+                virtualTop = true;
+            }
+            // if the neighbor is connect bottom or the site is, validate the virtualBottom
+            if (!virtualBottom && isconnectBottom[uf.find(index - n)] || isconnectBottom[uf.find(index)]) {
+                virtualBottom = true;
+            }
+            uf.union(index, index - n);
+        }
+        // union the site with down neighbor
+        if (row < n - 1 && siteopen[index + n]) {
+            //if the neighbor is full or site is full, validate the virtualTop
+            if (!virtualTop && isconnectTop[uf.find(index + n)] || isconnectTop[uf.find(index)]) {
+                virtualTop = true;
+            }
+            // if the neighbor is connect bottom or the site is, validate the virtualBottom
+            if (!virtualBottom && isconnectBottom[uf.find(index + n)] || isconnectBottom[uf.find(index)]) {
+                virtualBottom = true;
+            }
+            uf.union(index, index + n);
+        }
+
+        // set the status of the site
+        isconnectTop[uf.find(index)] = virtualTop;
+        isconnectBottom[uf.find(index)] = virtualBottom;
+
+        // check whether the system is percolation
+        if (!percolatesFlag && isconnectTop[uf.find(index)] && isconnectBottom[uf.find(index)]) {
+            percolatesFlag = true;
+        }
+    }
+
+    //Check whether the site (row, col) open
+    public boolean isOpen(int row, int col) {
+        check(row, col);
+        return siteopen[xyTo1D(row, col)];
+    }
+
+    //Check whether the site (row, col) full
+    public boolean isFull(int row, int col) {
+        check(row, col);
+        return isconnectTop[uf.find(xyTo1D(row, col))];
+    }
+
+    //return the number of open sites
+    public int numberOfOpenSites() {
+        return countOpen;
+    }
+
+    //return whether the system is percolate
+    public boolean percolates() {
+        return percolatesFlag;
+    }
+
+    //check the input N
+    public void check(int num) {
+        if (num <= 0) {
+            throw new IllegalArgumentException("N could not be less than 0");
+        }
+    }
+
+    //check whether the site is out of bounds
+    public void check(int x, int y) {
+        if (x < 0 || x >= n || y < 0 || y >= n) {
+            throw new IndexOutOfBoundsException("site must inside the grid");
+        }
+    }
+
+    //transform xy to 1D
+    public int xyTo1D(int x, int y) {
+        check(x, y);
+        return x * n + y;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,67 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
 
+    private final double mean;
+    private final double stddev;
+    private final double confidenceHi;
+    private final double confidenceLow;
+    private static final double CONFIDENCE_95 = 1.96;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        check(N);
+        check(T);
+
+        double[] results = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation uf = pf.make(N * N);
+            while (!uf.percolates()) {
+                // generate random sites in until percolates
+                int row = StdRandom.uniform(N); // Start from line 1
+                int col = StdRandom.uniform(N);
+                // regenerate site if it is already open
+                while (uf.isOpen(row, col)) {
+                    row = StdRandom.uniform(N);
+                    col = StdRandom.uniform(N);
+                }
+                uf.open(row, col);
+            }
+            results[i] = (double) uf.numberOfOpenSites() / (N * N * 1.0);
+        }
+
+        mean = StdStats.mean(results);
+        stddev = StdStats.stddev(results);
+        confidenceHi = mean + CONFIDENCE_95 * stddev / Math.sqrt(T);
+        confidenceLow = mean - CONFIDENCE_95 * stddev / Math.sqrt(T);
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return mean;
+    }
+
+    // sample standard deviation of percolation
+    public double stddev() {
+        return stddev;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return confidenceLow;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return confidenceHi;
+    }
+
+    public void check(int num) {
+        if (num <= 0) {
+            throw new IllegalArgumentException("The N or T should not be less than 0");
+        }
+    }
 }
